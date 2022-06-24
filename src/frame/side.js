bf97bd12075d05925d21422f4951356738693701
jmjeon0827 <jmjeon0827@gmail.com>
2022-06-24 16:25:16 +0900

    세션 체크로직 주석처리

--- src/frame/side.js
+++ src/frame/side.js
@@ -12,15 +12,18 @@ function Side() {
   const handleListItemClick = (event, index) => {
     //로그인 안되어 있으면 페이지 이동 막기.
     //테스트하실때는 강제로 아래 주석풀어서 세션스토리지에 넣고 사용하세요.
-    // sessionStorage.getItem('id', 'TEST@naver.com');
-    // let sessionStorage = window.sessionStorage;
-    // let id = sessionStorage.getItem('id');
-    // if (id === null || id === '' || id === 'GUEST') {
-    //   alert('로그인 먼저 진행해주세요.');
-    //   event.preventDefault();
-    // } else {
-    //   setSelectedIndex(index);
-    // }
+    //----------------------------------------------- ToDo. 아래 세션 체크 로직 변경해주세요. by JIN 2022-06-24
+    /*
+    sessionStorage.getItem('id', 'TEST@naver.com');
+    let sessionStorage = window.sessionStorage;
+    let id = sessionStorage.getItem('id');
+    if (id === null || id === '' || id === 'GUEST') {
+      alert('로그인 먼저 진행해주세요.');
+      event.preventDefault();
+    } else {
+      setSelectedIndex(index);
+    }
+    */
   };
   /* 메뉴 추가 */
   const itemList = [

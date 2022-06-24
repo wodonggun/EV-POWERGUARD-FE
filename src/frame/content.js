f76a4378f0df7586f06492fe4ef579e5d3911be6
jmjeon0827 <jmjeon0827@gmail.com>
2022-06-24 13:58:20 +0900

    react route test중입니다.

--- src/frame/content.js
+++ src/frame/content.js
@@ -5,10 +5,6 @@ import { Review } from '../pages/review';
 import { DriverLog } from '../pages/driverlog';
 import Home from '../pages/account/home';
 import SignIn from '../pages/account/signIn';
-import SignUp from '../pages/account/signUp';
-import SignEdit from '../pages/account/signEdit';
-import { Breakdown } from '../pages/breakdown';
-import { Bookmark } from '../pages/bookmark';
 
 function Content() {
   return (
@@ -19,11 +15,6 @@ function Content() {
         <Route path="/charger/:stationId" element={<Charger />}></Route>
         <Route path="/review" element={<Review />}></Route>
         <Route path="/driverlog" element={<DriverLog />}></Route>
-        <Route path="/signIn" element={<SignIn />}></Route>
-        <Route path="/signUp" element={<SignUp />}></Route>
-        <Route path="/signEdit" element={<SignEdit />}></Route>
-        <Route path="/breakdown" element={<Breakdown />}></Route>
-        <Route path="/bookmark" element={<Bookmark />}></Route>
       </Routes>
     </div>
   );

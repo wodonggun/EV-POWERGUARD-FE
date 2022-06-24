fe80b03f16312a5f884278304c505a49b0d19ed0
jmjeon0827 <jmjeon0827@gmail.com>
2022-06-24 14:19:40 +0900

    react route 테스트

--- src/frame/content.js
+++ src/frame/content.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { Station, Charger } from '../pages/station';
-import { Review } from '../pages/review';
-import { DriverLog } from '../pages/driverlog';
-import Home from '../pages/account/home';
-import SignIn from '../pages/account/signIn';
+import { Station } from '../pages/station';
 
 function Content() {
   return (

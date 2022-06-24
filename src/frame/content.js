67b5b3b39fae5618a015b90142a75b25e004b9ea
jmjeon0827 <jmjeon0827@gmail.com>
2022-06-24 14:32:08 +0900

    contect route 원복

--- src/frame/content.js
+++ src/frame/content.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { Station } from '../pages/station';
+import { Station, Charger } from '../pages/station';
+import { Review } from '../pages/review';
+import { DriverLog } from '../pages/driverlog';
+import Home from '../pages/account/home';
+import SignIn from '../pages/account/signIn';
+import SignUp from '../pages/account/signUp';
+import SignEdit from '../pages/account/signEdit';
+import { Breakdown } from '../pages/breakdown';
+import { Bookmark } from '../pages/bookmark';
 
 function Content() {
   return (
     <div className="App">
       <Routes>
+        <Route exact={true} path="/" element={<SignIn />}></Route>
         <Route path="/station" element={<Station />}></Route>
+        <Route path="/charger/:stationId" element={<Charger />}></Route>
+        <Route path="/review" element={<Review />}></Route>
+        <Route path="/driverlog" element={<DriverLog />}></Route>
+        <Route path="/signIn" element={<SignIn />}></Route>
+        <Route path="/signUp" element={<SignUp />}></Route>
+        <Route path="/signEdit" element={<SignEdit />}></Route>
+        <Route path="/breakdown" element={<Breakdown />}></Route>
+        <Route path="/bookmark" element={<Bookmark />}></Route>
       </Routes>
     </div>
   );
